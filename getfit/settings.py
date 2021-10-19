9ab154877f297345ebab0b1565a6e95ece89506a
Shivam Yadav <shivamyvc@gmail.com>
2021-10-19 22:30:42 +0530

    Chnage

--- getfit/settings.py
+++ getfit/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-# STATIC_ROOT= os.path.join(BASE_DIR, "staticfiles")
-STATIC_ROOT = 'staticfiles'
+
+MEDIA_ROOT= os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static/"),
