03e53c474518469e5b3fadf985c9dd2f5d51e0e6
Shivam Yadav <shivamyvc@gmail.com>
2021-10-19 21:04:14 +0530

    static

--- getfit/settings.py
+++ getfit/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-l9^oc6-qy+97z(93aa*6^dauxd46j+dlnez0gv)%33j(1yrc97'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
